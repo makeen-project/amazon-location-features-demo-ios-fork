67b7a2eed31bc9d7a4b716793af1430fdc6546e2
Denis Ryzhkov <denis.ryzhkov.dev@gmail.com>
2023-05-11 15:17:21 +0300

    Added additional handling for maybeLater button in the CommonDashboardView

--- LocationServices/LocationServices/Scenes/Tracking/Subviews/Tracking Dashboard/Cntroller/TrackingDashboard.swift
+++ LocationServices/LocationServices/Scenes/Tracking/Subviews/Tracking Dashboard/Cntroller/TrackingDashboard.swift
@@ -13,11 +13,14 @@ final class TrackingDashboardController: UIViewController {
     var trackingHistoryHandler: VoidHandler?
     var closeHandler: VoidHandler?
     
-    private var dashboardView: CommonDashboardView = CommonDashboardView(title: "Enable Tracking",
-                                                                         detail: "Enabling the feature will allow you to track your device and get notified when the device enters or exits any of your geofences.",
-                                                                         image: .trackingIcon,
-                                                                         iconBackgroundColor: .white,
-                                                                         buttonTitle: "Enable Tracking")
+    private var dashboardView = CommonDashboardView(
+        title: "Enable Tracking",
+        detail: "Enabling the feature will allow you to track your device and get notified when the device enters or exits any of your geofences.",
+        image: .trackingIcon,
+        iconBackgroundColor: .white,
+        buttonTitle: "Enable Tracking",
+        showMaybeLater: UIDevice.current.userInterfaceIdiom == .phone
+    )
     
     var viewModel: TrackingDashboardViewModelProcotol! {
         didSet {
@@ -35,7 +38,8 @@ final class TrackingDashboardController: UIViewController {
     private func setupViews() {
         self.view.addSubview(dashboardView)
         dashboardView.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
             $0.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
