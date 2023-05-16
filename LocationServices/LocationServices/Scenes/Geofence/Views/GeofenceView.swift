c93a883a9640aee2899f4d1a34816410f2c0485f
Zeeshan Sheikh <64203935+zeeshanmakeen@users.noreply.github.com>
2023-05-16 19:06:27 +0500

    Implemented dark and light logo color as per mapstyle

--- LocationServices/LocationServices/Scenes/Geofence/Views/GeofenceView.swift
+++ LocationServices/LocationServices/Scenes/Geofence/Views/GeofenceView.swift
@@ -120,6 +120,8 @@ final class GeofenceMapView: UIView {
     func reloadMap() {
         mapView.setupMapView()
         deselectAnnotation()
+        let mapName = UserDefaultsHelper.getObject(value: MapStyleModel.self, key: .mapStyle)
+        amazonMapLogo.tintColor = GeneralHelper.getAmazonMapLogo(mapImageType: mapName?.imageType)
     }
     
     func deselectAnnotation() {
