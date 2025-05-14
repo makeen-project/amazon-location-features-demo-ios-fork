ec65a947642cc356aba329dd2387d1ba91cc275f
Zeeshan Sheikh <zeeshan@makeen.io>
2025-05-14 16:37:17 +0500

    added NSLocalizedString in StringConstant

--- LocationServices/LocationServices/Constants/StringConstants.swift
+++ LocationServices/LocationServices/Constants/StringConstants.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+
 extension String {
     // LSFaux3DUserLocationAnnotationView
     
@@ -28,9 +29,7 @@ extension String {
     static let domainErrorLocalizedDescription = "The operation couldn’t be completed. (kCLErrorDomain error 0.)"
     static let testExpectationError = "expectation not matched after waiting"
     static let sessionExpiredError = "Session is expired. Please sign out and sign in back to continue access all features. Otherwise you could face unexpected behaviour in the app"
-    static let greatDistanceErrorTitle = "Distance is greater than 400 km"
-    static let greatDistanceErrorMessage = "Can't calculate via Esri, kindly switch to HERE provider"
-    static let invalidUrlError = "URL is invalid. Can't open it"
+   
     static let awsStackInvalidTitle = "Invalid AWS Stack"
     static let awsStackInvalidExplanation = "Stack is not invalid anymore or deleted, app will disconnect from AWS and restart"
 }
@@ -54,155 +53,158 @@ enum StringConstant {
     }
     static let developmentSchema = "https"
     
+    static let greatDistanceErrorTitle = NSLocalizedString("Distance is greater than 400 km", comment: "")
+    static let greatDistanceErrorMessage = NSLocalizedString("Can't calculate via Esri, kindly switch to HERE provider", comment: "")
+    static let invalidUrlError = NSLocalizedString("URL is invalid. Can't open it", comment: "")
     
     // strings:
-    static let directions = "Directions"
-    static let maybeLater = "Maybe later"
-    static let checkYourConnection = "Check your internet connection and try again"
-    static let amazonLocatinCannotReach = "Amazon Location can't reach the internet"
-    static let terminate = "Ok"
-    static let failedToCalculateRoute = "Failed to calculate route"
-    static let noInternetConnection = "No internet connection"
-    static let trackers = "Trackers"
-    static let trackingChangeToHere = "You can use any data provider except Esri for your asset management or device tracking use cases. If you want to use Esri for your asset management or tracking user case, please read terms and conditions."
-    static let enableTrackingDescription = "Enabling the feature will allow you to track your device and get notified when the device enters or exits any of your geofences."
+    static let directions = NSLocalizedString("Directions", comment: "")
+    static let maybeLater = NSLocalizedString("Maybe later", comment: "")
+    static let checkYourConnection = NSLocalizedString("Check your internet connection and try again", comment: "")
+    static let amazonLocatinCannotReach = NSLocalizedString("Amazon Location can't reach the internet", comment: "")
+    static let terminate = NSLocalizedString("Ok", comment: "")
+    static let failedToCalculateRoute = NSLocalizedString("Failed to calculate route", comment: "")
+    static let noInternetConnection = NSLocalizedString("No internet connection", comment: "")
+    static let trackers = NSLocalizedString("Trackers", comment: "")
+    static let trackingChangeToHere = NSLocalizedString("You can use any data provider except Esri for your asset management or device tracking use cases. If you want to use Esri for your asset management or tracking user case, please read terms and conditions.", comment: "")
+    static let enableTrackingDescription = NSLocalizedString("Enabling the feature will allow you to track your device and get notified when the device enters or exits any of your geofences.", comment: "")
     
-    static let startTracking = "Start Tracking"
-    static let stopTracking = "Stop Tracking"
-    static let startSimulation = "Try Trackers & Geofences simulation"
-    static let simulation = "Simulation"
-    static let trackersGeofences = "Trackers and Geofences"
-    static let trackersGeofencesHeader = "Tracking and Geofence simulation"
-    static let trackersGeofencesDetail = "Enter the Trackers simulation to view the path across Vancouver streets that crosses Geofences"
-    static let startTrackingSimulation = "Try Trackers & Geofences Simulation"
-    static let trackersDetail = "Visualize your location history on the map"
-    static let geofences = "Geofences"
-    static let geofencesDetail = "Define virtual boundaries around a specific area to detect entry and exit events"
-    static let notifications = "Notifications"
-    static let notificationsDetail = "Get geofence messages when you enter and leave locations"
-    static let routesNotifications = "Routes Notifications"
+    static let startTracking = NSLocalizedString("Start Tracking", comment: "")
+    static let stopTracking = NSLocalizedString("Stop Tracking", comment: "")
+    static let startSimulation = NSLocalizedString("Try Trackers & Geofences simulation", comment: "")
+    static let simulation = NSLocalizedString("Simulation", comment: "")
+    static let trackersGeofences = NSLocalizedString("Trackers and Geofences", comment: "")
+    static let trackersGeofencesHeader = NSLocalizedString("Tracking and Geofence simulation", comment: "")
+    static let trackersGeofencesDetail = NSLocalizedString("Enter the Trackers simulation to view the path across Vancouver streets that crosses Geofences", comment: "")
+    static let startTrackingSimulation = NSLocalizedString("Try Trackers & Geofences Simulation", comment: "")
+    static let trackersDetail = NSLocalizedString("Visualize your location history on the map", comment: "")
+    static let geofences = NSLocalizedString("Geofences", comment: "")
+    static let geofencesDetail = NSLocalizedString("Define virtual boundaries around a specific area to detect entry and exit events", comment: "")
+    static let notifications = NSLocalizedString("Notifications", comment: "")
+    static let notificationsDetail = NSLocalizedString("Get geofence messages when you enter and leave locations", comment: "")
+    static let routesNotifications = NSLocalizedString("Routes Notifications", comment: "")
     
-    static let emptyTrackingHistory = "No tracking history available"
+    static let emptyTrackingHistory = NSLocalizedString("No tracking history available", comment: "")
     
-    static let tracker = "Tracker"
-    static let entered = "Entered"
-    static let exited = "Exited"
-    static let exit = "Exit"
+    static let tracker = NSLocalizedString("Tracker", comment: "")
+    static let entered = NSLocalizedString("Entered", comment: "")
+    static let exited = NSLocalizedString("Exited", comment: "")
+    static let exit = NSLocalizedString("Exit", comment: "")
     
-    static let change = "Change"
+    static let change = NSLocalizedString("Change", comment: "")
     
-    static let go = "Go"
-    static let preview = "Preview"
-    static let info = "Info"
-    static let done = "Done"
+    static let go = NSLocalizedString("Go", comment: "")
+    static let preview = NSLocalizedString("Preview", comment: "")
+    static let info = NSLocalizedString("Info", comment: "")
+    static let done = NSLocalizedString("Done", comment: "")
     
-    static let locationPermissionDenied = "Location permission denied"
-    static let locationPermissionDeniedDescription = "Distance can't be calculated if location permission is not granted. Please enable location permission for Amazon Location from Settings"
-    static let esriDistanceError = "In DataSource Esri, all waypoints must be within 400km"
-    static let locationPermissionEnableLocationAction = "Enable Location"
-    static let locationPermissionAlertTitle = "Allow \"LocationServices\" to use your location"
-    static let locationPermissionAlertText = "Amazon Location will use your location to create a route to the selected location"
+    static let locationPermissionDenied = NSLocalizedString("Location permission denied", comment: "")
+    static let locationPermissionDeniedDescription = NSLocalizedString("Distance can't be calculated if location permission is not granted. Please enable location permission for Amazon Location from Settings", comment: "")
+    static let esriDistanceError = NSLocalizedString("In DataSource Esri, all waypoints must be within 400km", comment: "")
+    static let locationPermissionEnableLocationAction = NSLocalizedString("Enable Location", comment: "")
+    static let locationPermissionAlertTitle = NSLocalizedString("Allow \"LocationServices\" to use your location", comment: "")
+    static let locationPermissionAlertText = NSLocalizedString("Amazon Location will use your location to create a route to the selected location", comment: "")
     
     // location manager
-    static let locationManagerAlertTitle = "Allow \"LocationServices\" to use your location?"
-    static let locationManagerAlertText = "We need your location to detect your location in map"
-    static let cancel = "Cancel"
-    static let settigns = "Settings"
-    static let error = "Error"
-    static let warning = "Warning"
-    static let ok = "OK"
+    static let locationManagerAlertTitle = NSLocalizedString("Allow \"LocationServices\" to use your location?", comment: "")
+    static let locationManagerAlertText = NSLocalizedString("We need your location to detect your location in map", comment: "")
+    static let cancel = NSLocalizedString("Cancel", comment: "")
+    static let settigns = NSLocalizedString("Settings", comment: "")
+    static let error = NSLocalizedString("Error", comment: "")
+    static let warning = NSLocalizedString("Warning", comment: "")
+    static let ok = NSLocalizedString("OK", comment: "")
     
     // dispatch queue
-    static let dispatchReachabilityLabel = "Reachability"
+    static let dispatchReachabilityLabel = NSLocalizedString("Reachability", comment: "")
     
     // coordinate label text
-    static let coordinateLabelText = "50.54943, 30.21989"
-    static let timeLabelText = "11:22 pm"
+    static let coordinateLabelText = NSLocalizedString("50.54943, 30.21989", comment: "")
+    static let timeLabelText = NSLocalizedString("11:22 pm", comment: "")
     
     // Tracking Simulation
-    static let exitTracking = ""
-    static let exitTrackingAlertMessage = "Are you sure you want to exit simulation?"
+    static let exitTracking = NSLocalizedString("", comment: "")
+    static let exitTrackingAlertMessage = NSLocalizedString("Are you sure you want to exit simulation?", comment: "")
     
     enum AboutTab {
-        static let title = "More"
-        static let cellAttributionTitle = "Attribution"
-        static let cellLegalTitle = "Terms & Conditions"
-        static let cellVersionTitle = "Version"
-        static let cellHelpTitle = "Help"
+        static let title = NSLocalizedString("More", comment: "")
+        static let cellAttributionTitle = NSLocalizedString("Attribution", comment: "")
+        static let cellLegalTitle = NSLocalizedString("Terms & Conditions", comment: "")
+        static let cellVersionTitle = NSLocalizedString("Version", comment: "")
+        static let cellHelpTitle = NSLocalizedString("Help", comment: "")
     }
     
     enum About {
-        static let descriptionTitle = "By downloading, installing, or using the Amazon Location Demo App, you agree to the App's Terms & Conditions for use."
-        static let appTermsOfUse = "Terms & Conditions"
+        static let descriptionTitle = NSLocalizedString("By downloading, installing, or using the Amazon Location Demo App, you agree to the App's Terms & Conditions for use.", comment: "")
+        static let appTermsOfUse = NSLocalizedString("Terms & Conditions", comment: "")
         static let appTermsOfUseURL = termsAndConditionsURL
-        static let copyright = "© \(Calendar.current.component(.year, from: Date())), Amazon Web Services, Inc. or its affiliates. All rights reserved."
+        static let copyright = NSLocalizedString("© \(Calendar.current.component(.year, from: Date())), Amazon Web Services, Inc. or its affiliates. All rights reserved.", comment: "")
     }
     
     enum Tracking {
-        static let noTracking = "Device tracking inactive"
-        static let isTracking = "Device tracking is active"
+        static let noTracking = NSLocalizedString("Device tracking inactive", comment: "")
+        static let isTracking = NSLocalizedString("Device tracking is active", comment: "")
     }
     
     enum TabBar {
-        static let explore: String = "Navigate"
-        static let tracking: String = "Trackers"
-        static let settings: String = "Settings"
-        static let about: String = "More"
+        static let explore: String = NSLocalizedString("Navigate", comment: "")
+        static let tracking: String = NSLocalizedString("Trackers", comment: "")
+        static let settings: String = NSLocalizedString("Settings", comment: "")
+        static let about: String = NSLocalizedString("More", comment: "")
     }
     
     enum NotificationsInfoField {
-        static let geofenceIsHidden = "geofenceIsHidden"
-        static let mapStyleIsHidden = "mapStyleIsHidden"
-        static let directionIsHidden = "directionIsHidden"
+        static let geofenceIsHidden = NSLocalizedString("geofenceIsHidden", comment: "")
+        static let mapStyleIsHidden = NSLocalizedString("mapStyleIsHidden", comment: "")
+        static let directionIsHidden = NSLocalizedString("directionIsHidden", comment: "")
     }
     
-    static let units = "Units"
-    static let dataProvider = "Data Provider"
-    static let mapStyle = "Map style"
-    static let defaultRouteOptions = "Default route options"
-    static let partnerAttributionTitle = "Partner Attribution"
-    static let partnerAttributionHEREDescription = "© AWS, HERE"
-    static let softwareAttributionTitle = "Software Attribution"
-    static let softwareAttributionDescription = "Click learn more for software attribution"
-    static let learnMore = "Learn More"
-    static let attribution = "Attribution"
-    static let about = "More"
-    static let version = "Version"
-    static let welcomeTitle = "Welcome to\nAmazon Location Demo"
-    static let continueString = "Continue"
-    static let avoidTolls = "Avoid tolls"
-    static let avoidFerries = "Avoid ferries"
-    static let avoidUturns = "Avoid uturns"
-    static let avoidTunnels = "Avoid tunnels"
-    static let avoidDirtRoads = "Avoid dirt roads"
-    static let myLocation = "My Location"
-    static let appVersion = "App version: "
-    static let termsAndConditions = "Terms & Conditions"
-    //static let disconnect = "Disconnect"
-    static let demo = "Demo"
-    static let routeOverview = "Route Overview"
+    static let units = NSLocalizedString("Units", comment: "")
+    static let dataProvider = NSLocalizedString("Data Provider", comment: "")
+    static let mapStyle = NSLocalizedString("Map style", comment: "")
+    static let defaultRouteOptions = NSLocalizedString("Default route options", comment: "")
+    static let partnerAttributionTitle = NSLocalizedString("Partner Attribution", comment: "")
+    static let partnerAttributionHEREDescription = NSLocalizedString("© AWS, HERE", comment: "")
+    static let softwareAttributionTitle = NSLocalizedString("Software Attribution", comment: "")
+    static let softwareAttributionDescription = NSLocalizedString("Click learn more for software attribution", comment: "")
+    static let learnMore = NSLocalizedString("Learn More", comment: "")
+    static let attribution = NSLocalizedString("Attribution", comment: "")
+    static let about = NSLocalizedString("More", comment: "")
+    static let version = NSLocalizedString("Version", comment: "")
+    static let welcomeTitle = NSLocalizedString("Welcome to\nAmazon Location Demo", comment: "")
+    static let continueString = NSLocalizedString("Continue", comment: "")
+    static let avoidTolls = NSLocalizedString("Avoid tolls", comment: "")
+    static let avoidFerries = NSLocalizedString("Avoid ferries", comment: "")
+    static let avoidUturns = NSLocalizedString("Avoid uturns", comment: "")
+    static let avoidTunnels = NSLocalizedString("Avoid tunnels", comment: "")
+    static let avoidDirtRoads = NSLocalizedString("Avoid dirt roads", comment: "")
+    static let myLocation = NSLocalizedString("My Location", comment: "")
+    static let appVersion = NSLocalizedString("App version: ", comment: "")
+    static let termsAndConditions = NSLocalizedString("Terms & Conditions", comment: "")
+    //static let disconnect = NSLocalizedString("Disconnect", comment: "")
+    static let demo = NSLocalizedString("Demo", comment: "")
+    static let routeOverview = NSLocalizedString("Route Overview", comment: "")
     
-    static let viewRoute = "View Route"
-    static let hideRoute = "Hide Route"
+    static let viewRoute = NSLocalizedString("View Route", comment: "")
+    static let hideRoute = NSLocalizedString("Hide Route", comment: "")
     
-    static let trackingNotificationTitle = "Amazon Location"
+    static let trackingNotificationTitle = NSLocalizedString("Amazon Location", comment: "")
 
-    static let arrivalCardTitle = "You've arrived!"
-    static let poiCardSchedule = "Schedule"
-    static let language = "Language"
-    static let politicalView = "Political view"
-    static let mapRepresentation = "Map representation for different countries"
-    static let mapLanguage = "Map Language"
-    static let selectLanguage = "Select Language"
-    static let leaveNow = "Leave now"
-    static let leaveAt = "Leave at"
-    static let arriveBy = "Arrive by"
-    static let routeOptions = "Route Options"
-    static let options = "Options"
-    static let selected = "Selected"
-    static let routesActive = "routes active"
-    static let politicalLight = "Light"
-    static let politicalDark = "Dark"
+    static let arrivalCardTitle = NSLocalizedString("You've arrived!", comment: "")
+    static let poiCardSchedule = NSLocalizedString("Schedule", comment: "")
+    static let language = NSLocalizedString("Language", comment: "")
+    static let politicalView = NSLocalizedString("Political view", comment: "")
+    static let mapRepresentation = NSLocalizedString("Map representation for different countries", comment: "")
+    static let mapLanguage = NSLocalizedString("Map Language", comment: "")
+    static let selectLanguage = NSLocalizedString("Select Language", comment: "")
+    static let leaveNow = NSLocalizedString("Leave now", comment: "")
+    static let leaveAt = NSLocalizedString("Leave at", comment: "")
+    static let arriveBy = NSLocalizedString("Arrive by", comment: "")
+    static let routeOptions = NSLocalizedString("Route Options", comment: "")
+    static let options = NSLocalizedString("Options", comment: "")
+    static let selected = NSLocalizedString("Selected", comment: "")
+    static let routesActive = NSLocalizedString("routes active", comment: "")
+    static let politicalLight = NSLocalizedString("Light", comment: "")
+    static let politicalDark = NSLocalizedString("Dark", comment: "")
     
     // Languages
     static let deutsch = "Deutsch"
@@ -218,4 +220,5 @@ enum StringConstant {
     static let arabic = "العربية"
     static let hebrew = "עברית"
     static let hindi = "हिन्दी"
+
 }
