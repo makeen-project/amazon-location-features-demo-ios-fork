0b8b0e7a2af229f67ae100b8180f8f306f7f0cb5
Zeeshan Sheikh <zeeshan@makeen.io>
2025-07-10 13:33:34 +0500

    Revert "Update AmazonLocationLabel.swift"
    This reverts commit 1b3039bd9cb9cf1742390e025430f4ea4f8966b1.

--- LocationServices/LocationServices/Views/Common/AmazonLocationLabel.swift
+++ LocationServices/LocationServices/Views/Common/AmazonLocationLabel.swift
@@ -39,7 +39,9 @@ final class AmazonLocationLabel: UILabel {
     ) {
         self.text = labelText
         self.font = font
-        self.applyLocaleDirection()
+        if textAlignment == .left {
+            self.applyLocaleDirection()
+        }
         if isMultiline {
             self.numberOfLines = 0
         }
